4386d969c04be8843ef204264dc644b0fe0858e0
dwikanur <67049247+dwikanur@users.noreply.github.com>
2020-11-21 17:55:44 +0700

    add stepper

--- lib/ui/page/input_sarana_prasarana.dart
+++ lib/ui/page/input_sarana_prasarana.dart
@@ -14,108 +14,92 @@ class InputSaranaPrasaranaPage extends StatefulWidget {
 }
 
 class _InputSaranaPrasaranaPageState extends State<InputSaranaPrasaranaPage> {
+
+  int _currentStep = 0;
+  bool complete = false;
+  StepperType stepperType = StepperType.horizontal;
+
+  next() {
+      _currentStep + 1 != _stepper().length
+          ? goTo(_currentStep + 1)
+          : setState(() => complete = true);
+    }
+
+    cancel() {
+      if (_currentStep > 0) {
+        goTo(_currentStep - 1);
+      }
+    }
+
+    goTo(int step) {
+      setState(() => _currentStep = step);
+    }
+
+    StepperType _stepperType = StepperType.vertical;
+
+    switchStepType(){
+      setState(() => _stepperType == StepperType.vertical
+          ? _stepperType = StepperType.horizontal
+          : _stepperType = StepperType.vertical);
+    }
+
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Scaffold(
-        floatingActionButton:
-        Padding(
-          padding: const EdgeInsets.only(bottom: 20),
-          child: FloatingActionButton.extended(
-            onPressed: () {},
-            elevation: 3,
-            backgroundColor: greenColors,
-            label: Container(
-              width: SizeConfig.screenWidth / 1.3,
-              child: Text(
-                "SUBMIT",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16),
-              ),
-            ),
-          ),
-        ),
-    floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      body: Container(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                width: SizeConfig.screenWidth,
-                height: SizeConfig.screenHight / 5,
-                color: blueColors,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(height: SizeConfig.screenHight / 18,),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 4.0),
-                      child: Text(
-                        'Form Input Data Sarana \n Prasarana',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                color: Colors.grey[200],
-                width: SizeConfig.screenWidth,
-                height: 110,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 12.0, top: 8.0),
-                      child: Text(
-                        'Ketua Pelaksana BPDB Prov, Aceh, Kab Aceh Singkil',
-                        style: TextStyle(
-                            color: abuAbu, fontSize: 12),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 12.0, top: 8.0),
-                      child: Text('Atung Nugroho',
-                        style: TextStyle(
-                            color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 12.0, top: 8.0),
-                      child: Text('Alamat Kantor',
-                        style: TextStyle(color: abuAbu, fontSize: 12),),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 12.0, top: 8.0),
-                      child: Text('Jl. Putri Tunggal No 98',
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
+      appBar: AppBar(
+        title: Text('mencoba'),
       ),
+      body: Stepper(
+        steps: _stepper(),
+        physics: ClampingScrollPhysics(),
+        currentStep: this._currentStep,
+        onStepTapped: (step) => goTo(step),
+        onStepContinue: next,
+        onStepCancel: cancel,
+        type: _stepperType,
+      ),
+      floatingActionButton:
+      FloatingActionButton(
+        onPressed: switchStepType,
+        backgroundColor: blueColors,
+        child: Icon(Icons.swap_horizontal_circle, color: Colors.white,),),
     );
   }
+
+  List<Step> _stepper(){
+    List<Step> _steps = [
+      Step(title: Text('Name'),
+          content: Column(
+            children: [
+              TextField(),
+            ],
+          ),
+        isActive: _currentStep >= 0,
+        state: StepState.indexed
+      ),
+      Step(title: Text('No'),
+        content: Column(
+          children: [
+            TextField(),
+          ],
+        ),
+        isActive: _currentStep >= 1,
+        state: StepState.disabled
+      ),
+      Step(title: Text('Name'),
+        content: Column(
+          children: [
+            TextField(),
+          ],
+        ),
+          isActive: _currentStep >= 2,
+          state: StepState.disabled
+      ),
+    ];
+    return _steps;
+  }
+
 }
 
   // String namaKendaraan = "-- Pilih --";
