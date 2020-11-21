0a7e2d29dfb795816e3669eca981968bb8a4099b
bayunugroho404 <baay404@hmail.com>
2020-11-21 16:36:05 +0700

    remove donate

--- lib/ui/page/covid_page/pannels/infoPanel.dart
+++ lib/ui/page/covid_page/pannels/infoPanel.dart
@@ -36,29 +36,32 @@ class InfoPanel extends StatelessWidget {
               ),
             ),
           ),
-          GestureDetector(
-            onTap: () {
-              launch('https://covid19responsefund.org/');
-            },
-            child: Container(
-              padding: EdgeInsets.symmetric(vertical: 12, horizontal: 10),
-              margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-              color: Colors.black,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text(
-                    'DONATE',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18),
-                  ),
-                  Icon(
-                    Icons.arrow_forward,
-                    color: Colors.white,
-                  )
-                ],
+          Visibility(
+            visible: false,
+            child: GestureDetector(
+              onTap: () {
+                launch('https://covid19responsefund.org/');
+              },
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 12, horizontal: 10),
+                margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                color: Colors.black,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'DONATE',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18),
+                    ),
+                    Icon(
+                      Icons.arrow_forward,
+                      color: Colors.white,
+                    )
+                  ],
+                ),
               ),
             ),
           ),
